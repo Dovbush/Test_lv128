032d5b72d3ddedbbe031c9d46ddf7301d626a43a
Alive-web <alivelp@gmail.com>
2014-10-13 10:06:30 +0300

    add view

---
+++ mysqlview.py
@@ -0,0 +1,20 @@
+import bufer
+
+def show_tab (di):
+	for i in di:
+		print di[i]
+
+""" test bufer block """
+A = bufer.tabl_products ()
+
+show_tab (A.product_row("UnitPrice", "=", "2.5") )
+
+print "####################"
+
+B = bufer.tabl_categories ()
+show_tab ( B.categories_row("CategoryID", "=", "1") )
+
+#My test
+
+c=bufer.tabl_region()
+c.print_region()
